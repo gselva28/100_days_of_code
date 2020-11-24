6d5bf2b54d3662c353e0ef2d6e01fea6cd8e62cd
GSelvakumar <gselvakumarofficial@gmail.com>
2020-11-24 12:25:42 +0530

    modified

--- DAY 4/pick_pay_bill.py
+++ DAY 4/pick_pay_bill.py
@@ -3,7 +3,18 @@ import random
 test_seed = int(input("Create a seed number: "))
 random.seed(test_seed)
 
-nameAsCSV = input("Give me everybody's names, separated by a comma. ")
+nameAsCSV = input("Give me everybody's names, separated by a comma. \n")
 names = nameAsCSV.split(", ")
 
-print(names)
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+random_choice_name = names[random_choice]
+
+print(f"{random_choice_name} is going to buy the meal today!")
+
+# In order to reduce the code this choice method also can be used.
+# random_choice = random.choice(names)
+# print(f"{random_choice} is going to buy the meal today!")
+
+
