dec792399ab8ef73c1fb5db73ed2f94c9cbadc45
GSelvakumar <gselvakumarofficial@gmail.com>
2020-11-29 21:41:09 +0530

    100 days of code

---
+++ DAY 10/title_str.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+    formated_fname = f_name.title()
+    formated_lname = l_name.title()
+    return f"{formated_fname} {formated_lname}"
+
+print(format_name("sElvA", "KUMAR"))
