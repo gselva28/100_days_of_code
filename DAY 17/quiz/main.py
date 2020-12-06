249452295fbc376df705eaf192d683d122532f3c
GSelvakumar <gselvakumarofficial@gmail.com>
2020-12-06 12:39:13 +0530

    100 days of code

--- DAY 17/quiz/main.py
+++ DAY 17/quiz/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -9,4 +10,12 @@ for question in question_data:
     question_bank.append(new_question)
 print(question_bank[0].text)
 print(question_bank[0].answer)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz_score}/{quiz.question_number}")
 
