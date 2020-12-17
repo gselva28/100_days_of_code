47048901f464f0ea3971b15aeb14c71fd1db7c85
GSelvakumar <gselvakumarofficial@gmail.com>
2020-12-17 22:08:01 +0530

    100 days of code

--- DAY 20/snake game/main.py
+++ DAY 20/snake game/main.py
@@ -1,24 +1,20 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Arcade Snake Game")
+screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_on = True
 while game_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
 
+    snake.move()
 
 screen.exitonclick()
