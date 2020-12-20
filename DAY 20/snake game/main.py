cc88063ca6716ce7670d76e337e1f0bae79fcc86
GSelvakumar <gselvakumarofficial@gmail.com>
2020-12-20 22:48:45 +0530

    100 days of code

--- DAY 20/snake game/main.py
+++ DAY 20/snake game/main.py
@@ -1,6 +1,7 @@
 import turtle as t
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = t.Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,5 +29,20 @@ while game_on:
     #detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
+
+    #detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
--- DAY 20/snake game/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-screen.onkey(snake.down, "Down")
-# screen.onkey(snake.left, "Left")
-# screen.onkey(snake.right, "Right")
